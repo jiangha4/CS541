f10e72ba2ede8669c13dff4f0bc7735ecd89294f
David Jiang <davidjiang.haohan@gmail.com>
2019-02-03 09:41:18 -0800

    parity and class functions

--- N-Puzzle/grid.py
+++ N-Puzzle/grid.py
@@ -1,32 +1,55 @@
-class square(object):
+class Square(object):
     def __init__(self, id):
         self.id = id
 
+    def __gt__(self, other):
+        if int(self.id) == -1:
+            return False
+        if int(other.id) == -1:
+            return False
+        return int(self.id) > int(other.id)
+
     def __repr__(self):
+        if self.id == '-1':
+            return '[b]'
         return '[' + self.id + ']'
 
-
-class grid(object):
-    def __init__(self, size,numList):
-        self.numList = numList
-        self.grid = self.createGrid()
+class Grid(object):
+    def __init__(self, size, numlist):
+        self.numList = numlist
+        self.grid = self.create_grid()
         self.size = size
+        self.length = len(self.grid)
+        self.parity = self.parity()
 
-    def createGrid(self):
+    def create_grid(self):
         grid = list()
         for elem in self.numList:
-            grid.append(square(elem))
+            if elem == 'b':
+                grid.append(Square('-1'))
+            else:
+                grid.append(Square(elem))
         return grid
+
+    def parity(self):
+        parity = 0
+        for i in range(0, self.length):
+            cur = self.grid[i]
+            for j in range(i+1, self.length):
+                if cur > self.grid[j]:
+                    parity = parity + 1
+        return parity
 
     def __repr__(self):
         rep = ''
         counter = 0
-        for i in range(0, len(self.grid)):
+        for i in range(0, self.length):
             counter = counter + 1
             rep = rep + str(self.grid[i])
             if counter%self.size == 0:
                 rep = rep + '\n'
         return rep
+
 
 if __name__ == '__main__':
     testList = ['1', '2', '3', '4', '5', '6', '7', '8', 'b']
--- N-Puzzle/search.py
+++ N-Puzzle/search.py
@@ -1,12 +1,36 @@
-from grid import grid
+from grid import Grid
+import argparse
 
-def bestFirstSearch():
+class Node(object):
+    def __init__(self):
+        self.state = None
+        self.parent = None
+        self.action = None
+        self.path_cost = None
+
+
+class SearchTree(object):
+    def __init__(self):
+        pass
+
+
+def best_first_search(start_state):
+    print(start_state.parity)
+
+def a_star_search():
     pass
 
-def aStarSearch():
-    pass
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-i', '--init', type=str, action='store',
+                        help='The initial puzzle state')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    testList = ['1', '2', '3', '4', '5', '6', '7', '8', 'b']
-    a = grid(3, testList)
-    print(a)
+    arg = argument_parser()
+    start_state = Grid(3, list(arg.init))
+    print(start_state)
+    best_first_search(start_state)
+
