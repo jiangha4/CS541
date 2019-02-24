5edaf077a2bc4cc526a85259447651e412d0aa33
David Jiang <david.jiang@hp.com>
2019-02-24 11:18:54 -0800

    adding graphing to main

--- 8-Queen/main.py
+++ 8-Queen/main.py
@@ -1,5 +1,4 @@
 import argparse
-import matplotlib
 from helpers.population import *
 from helpers.selection import *
 from helpers.myheapq import *
@@ -9,6 +8,8 @@ def argument_parser():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-p', '--population', type=int, action='store',
                         help='The initial population size')
+    parser.add_argument('-g', '--graph', action='store_true',
+                        help='Show graphs. Must have matplotlib package installed')
     return parser.parse_args()
 
 
@@ -36,15 +37,30 @@ def check_goal(population):
     return None
 
 
-def main():
-    pop, total_fitness = generate_population(1000)
+def make_graph(generations, avg_fitness):
+    import matplotlib.pyplot as plt
+    xaxis = [x for x in range(0, generations)]
+    plt.plot(xaxis, avg_fitness)
+    plt.ylabel('Average fitness')
+    plt.xlabel('Generation')
+    plt.show()
+
+def main(args):
+    pop, total_fitness = generate_population(10)
     generations = 1000
+    fitness_per_generation = [total_fitness/len(pop)]
     for i in range(0, generations):
         population, total_fitness = reproduce(pop, total_fitness)
-        goal = check_goal(population)
-        if goal:
-            print(goal, i)
-            return goal
+        if args.graph:
+            fitness_per_generation.append(total_fitness/len(population))
+        #goal = check_goal(population)
+        #if goal:
+        #    print(goal, i)
+        #    return goal
+
+    if args.graph:
+        make_graph(generations, fitness_per_generation)
 
 if __name__ == '__main__':
-    main()
+    args = argument_parser()
+    main(args)
